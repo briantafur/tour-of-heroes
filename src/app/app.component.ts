6fe70f3be2a22ae80785b74e8bdfeddb2499763c
Brian David Tafur Londoño <briantafur89@hotmail.com>
2017-11-03 11:08:21 -0500

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Hero } from './Hero';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1094,
+    name: 'Brian'
+  };
+  selectedHero: Hero;
+  HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+  ];
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
 }
---
+++ src/app/hero-datail/hero-datail.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../Hero';
+
+
+@Component({
+  selector: 'app-hero-datail',
+  templateUrl: './hero-datail.component.html',
+  styleUrls: ['./hero-datail.component.css']
+})
+export class HeroDatailComponent implements OnInit {
+
+  @Input() hero: Hero;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
